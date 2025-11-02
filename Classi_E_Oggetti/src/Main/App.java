8cee4d36b35565d4138883b9a7c7ff671ab60456
GiovanniUrso <121301413+GiovanniUrso@users.noreply.github.com>
2025-11-02 18:36:45 +0100

    changes.

---
+++ Classi_E_Oggetti/src/Main/App.java
@@ -0,0 +1,16 @@
+package Main;
+
+public class App {
+
+	public static void main(String[] args) {
+	
+		Persona davide = new Persona();
+		davide.nome = "Davide";
+		davide.altezza = 1.78;
+		davide.coloreOcchi = "Marrone";
+		davide.isMarried = false;
+		
+		davide.Saluta();
+	}
+
+}
---
+++ Classi_E_Oggetti/src/Main/Persona.java
@@ -0,0 +1,22 @@
+package Main;
+
+public class Persona {
+	
+	String nome;
+	double altezza;
+	String coloreOcchi;
+	int peso;
+	int età;
+	String professione;
+	boolean isMarried;
+	
+	
+	
+	public void Saluta() {
+		
+		System.out.println("Ciao sono:" + " " + this.nome);
+	}
+	
+	
+
+}
